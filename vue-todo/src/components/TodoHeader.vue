545b0afdd17ed206a0edf8b95435484f2a086793
myung9 <ymgu0709@gmail.com>
2021-03-11 15:03:38 +0900

    a

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- html -->
+  <header>
+      <h1>
+          TODO!
+      </h1>
+  </header>
+</template>
+
+</script>
+
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <!-- v-model : input에 입력된 text값을 동적으로 사용가능하게함 -->
+      <input type="text" v-model="newTodoItem">
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            newTodoItem: ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
