772ee3a89f08e2389b8bd5d205cd61f885ca5db2
myung9 <ymgu0709@gmail.com>
2021-03-18 11:07:18 +0900

    toggle

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -19,8 +19,12 @@ export default {
     methods: {
         addTodo: function(){
             console.log(this.newTodoItem)
-            localStorage.setItem(this.newTodoItem, this.newTodoItem);
-            this.clearInput();
+            if (this.newTodoItem !== ''){
+                var obj = {completed: false, item: this.newTodoItem};
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+                // localStorage.setItem(this.newTodoItem, obj)
+                this.clearInput();
+            }
         },
         clearInput: function(){
             this.newTodoItem = '';
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-      TodoList
       <ul>
         <!-- vscode상에서는 v-bind도 명시 해주어야 에러체크안남 -->
-        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
-          {{ todoItem }}
+        <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+          <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
+          v-on:click="toggleComplete(todoItem, index)"></i>
+          <span v-bind:class="{textCompleted: todoItem.completed}">
+            {{ todoItem.item }}
+          </span>
+          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+            <i class="fas fa-trash-alt"></i>
+          </span>
         </li>
       </ul>
   </div>
@@ -22,15 +28,60 @@ export default {
     if(localStorage.length > 0){
       for ( var i = 0; i < localStorage.length; i++){
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          this.todoItems.push(localStorage.key(i));
-          console.log(localStorage.key(i));
+          // this.todoItems.push(localStorage.key(i));
+          // console.log(localStorage.key(i));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
       }
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index){
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem, index){
+        console.log(todoItem, index);
+        todoItem.completed = !todoItem.completed;
+        // 로컬 스토리지에 데이터를 갱신
+        localStorage.removeItem(todoItem.item);
+        localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   }
 }
 </script>
 
 <style>
-
+  ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    border-radius: 5px;
+  }
+  .removeBtn{
+    margin-left: auto;
+    color: #de4343
+  }
+  .checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted{
+    color: #b3adad;
+  }
+  .textCompleted{
+    text-decoration: line-through;
+    color: #b3adad;
+  }
 </style>
