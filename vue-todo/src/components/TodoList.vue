6eace206981dac21deef86f07bb78be255eb5e93
m9 <ymgu0709@gmail.com>
2021-04-05 23:23:32 +0900

    list transition

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-      <ul>
+      <transition-group name="list" tag="ul">
+      
         <!-- vscode상에서는 v-bind도 명시 해주어야 에러체크안남 -->
         <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
           <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
@@ -11,7 +12,7 @@
             <i class="fas fa-trash-alt"></i>
           </span>
         </li>
-      </ul>
+      </transition-group>
   </div>
 </template>
 
@@ -32,35 +33,49 @@ export default {
 </script>
 
 <style>
-  ul{
-    list-style-type: none;
-    padding-left: 0px;
-    margin-top: 0;
-    text-align: left;
-  }
-  li{
-    display: flex;
-    min-height: 50px;
-    height: 50px;
-    line-height: 50px;
-    margin: 0.5rem 0;
-    padding: 0 0.9rem;
-    border-radius: 5px;
-  }
-  .removeBtn{
-    margin-left: auto;
-    color: #de4343
-  }
-  .checkBtn{
-    line-height: 45px;
-    color: #62acde;
-    margin-right: 5px;
-  }
-  .checkBtnCompleted{
-    color: #b3adad;
-  }
-  .textCompleted{
-    text-decoration: line-through;
-    color: #b3adad;
-  }
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  border-radius: 5px;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+/* 리스트 아이템 트렌지션 효과 */
+/* https://kr.vuejs.org/v2/guide/transitions.html#%EB%A6%AC%EC%8A%A4%ED%8A%B8%EC%9D%98-%EC%A7%84%EC%9E%85-%EC%A7%84%EC%B6%9C-%ED%8A%B8%EB%9E%9C%EC%A7%80%EC%85%98 */
+/* .list-item {
+  display: inline-block;
+  margin-right: 10px;
+} */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
